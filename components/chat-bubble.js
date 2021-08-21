a3674fb6e2165eb9eda62bfb25e3e4fc9c87b31e
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-08-21 04:19:03 +0000

    added typing indicators

--- components/chat-bubble.js
+++ components/chat-bubble.js
@@ -1,12 +1,38 @@
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
+import random from 'lodash/random'
 
-const ChatBubble = ({ type, message }) => (
-  <div className={`cv-auto ${type === 'recieved' ? '' : 'text-right'}`}>
-    <p className={`inline-block p-2 my-2 leading-normal text-white rounded-lg break-all ${type === 'recieved' ? 'bg-pink-700 rounded-bl-none' : 'bg-blue-700 rounded-br-none'}`}>
-      {message}
-    </p>
-  </div>
+const Dot = () => (
+  <span
+    role="img"
+    className="animate-pulse inline-block bg-white rounded-full w-2 h-2 m-1 motion-reduce:animate-none"
+  ></span>
 )
+
+const ChatBubble = ({ type, message }) => {
+  const [typingIndicator, setTypingIndicator] = useState(type === 'received')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTypingIndicator(false)
+    }, random(800, 1200))
+    
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <div className={`cv-auto ${type === 'received' ? '' : 'text-right'}`}>
+      <p
+        className={`inline-block p-2 my-2 leading-normal text-white rounded-lg break-all ${
+          type === "received"
+            ? 'bg-pink-700 rounded-bl-none'
+            : 'bg-blue-700 rounded-br-none'
+        }`}
+      > {typingIndicator ? <><Dot /><Dot /><Dot /></> : message}
+      </p>
+    </div>
+  )
+}
 
 ChatBubble.defaultProps = {
   type: 'sent',
@@ -14,7 +40,7 @@ ChatBubble.defaultProps = {
 }
 
 ChatBubble.propTypes = {
-  type: PropTypes.oneOf(['sent', 'recieved']),
+  type: PropTypes.oneOf(['sent', 'received']),
   message: PropTypes.string,
 }
 
