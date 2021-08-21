7d784dc905b051de530225f79d7237af8f337914
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-08-21 03:32:02 +0000

    credited

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,45 @@
-import { useEffect, useState } from 'react'
-import Layout from '../components/layout'
-import ChatBubble from '../components/chat-bubble'
-import MessageComposer from '../components/message-composer'
-import last from 'lodash/last'
-import has from 'lodash/has'
-import ai from '../utils/ai'
+import { useCallback, useEffect, useState } from "react";
+import Layout from "../components/layout";
+import ChatBubble from "../components/chat-bubble";
+import MessageComposer from "../components/message-composer";
+import last from "lodash/last";
+import has from "lodash/has";
+import ai from "../utils/ai";
 
 const Index = () => {
-  const [chatHistory, setChatHistory] = useState([])
-  const addToChatHistory = (type, message) => {
-    setChatHistory([...chatHistory, { message, type }])
-  }
+  const [chatHistory, setChatHistory] = useState([]);
+  const addToChatHistory = useCallback(
+    (type, message) => {
+      setChatHistory([...chatHistory, { message, type }]);
+    },
+    [chatHistory]
+  );
 
   useEffect(() => {
-    const lastMessage = last(chatHistory)
-    if (has(lastMessage, 'type') && lastMessage.type === 'sent') {
-      addToChatHistory('recieved', ai(lastMessage.message))
+    const lastMessage = last(chatHistory);
+    if (has(lastMessage, "type") && lastMessage.type === "sent") {
+      addToChatHistory("received", ai(lastMessage.message));
     }
 
     let timeout = window.setTimeout(() => {
-      document.querySelector('form').scrollIntoView(true)
-    } , 1)
+      document.querySelector("form").scrollIntoView(true);
+    }, 1);
 
     return () => {
-      clearTimeout(timeout)
-    }
-  }, [chatHistory, addToChatHistory])
-
+      clearTimeout(timeout);
+    };
+  }, [chatHistory, addToChatHistory]);
 
   return (
     <Layout>
       <h1 className="text-3xl font-bold leading-loose">Improv Dungeon</h1>
-      <p className="mb-3">Write out your scenario and the AI will play along!</p>
+      <p className="mb-3">
+        Write out your scenario and the AI will play along!
+        <br />
+        <em>
+          made with 💖 by <a href="https://melaniekat.com">melanie kat</a>
+        </em>
+      </p>
 
       {chatHistory.map(({ message, type }, i) => (
         <ChatBubble key={i} type={type} message={message} />
@@ -39,7 +47,7 @@ const Index = () => {
 
       <MessageComposer onSubmit={addToChatHistory} />
     </Layout>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
