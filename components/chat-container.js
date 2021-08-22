3041e64784082fba26c547d81560ce6f0a17a6b4
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-08-22 05:40:00 +0000

    smarter chat window sizing

--- components/chat-container.js
+++ components/chat-container.js
@@ -2,6 +2,26 @@ import { useEffect, useRef } from 'react'
 
 const ChatContainer = ({ children }) => {
   const chatContainerRef = useRef(null)
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { current } = chatContainerRef
+      if (current) {
+        const currentOffset = current.getBoundingClientRect().top
+        const currentFormSize = document.querySelector('form').offsetHeight
+        const largeScreenPadding = window.matchMedia('(min-width: 640px)').matches ? 100 : 0
+        current.style.height = `${window.innerHeight - currentOffset - currentFormSize - largeScreenPadding}px`
+      }
+    }
+    if (typeof window !== 'undefined') {
+      handleResize()
+      window.addEventListener('resize', handleResize);
+    }
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
+
   useEffect(() => {
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
   },[chatContainerRef, children])
