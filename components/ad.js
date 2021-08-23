f6aabc674ed31ca7554a51c81cfcced2b3364b55
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-08-22 21:41:26 -0700

    fallback for failed api

--- components/ad.js
+++ components/ad.js
@@ -1,13 +1,15 @@
-const Ad = ({ name, url, description, img }) => (
+const Ad = ({ name, url }) => (
   <p className="my-4 text-black dark:text-white sm:text-center">
     <a href={'https://melkat.deals/'} className={'font-bold text-black dark:text-white'}>
       melkat.deals
     </a>
-    : Support my work by giving{' '}
-    <a href={url} className={'font-bold'}>
-      {name}
-    </a>
-    {' '}a try.
+    {name && url ? (<>
+      : Support my work by giving{' '}
+      <a href={url} className={'font-bold'}>
+        {name}
+      </a>
+      {' '}a try.
+    </>) : ': Support my work by checking these links.'}
   </p>
 )
 
